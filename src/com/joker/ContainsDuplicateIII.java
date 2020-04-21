9e748195ada7140820c3b9b9adb20c7cdbbda965
张鹏翔 <joker_runner@qq.com>
2020-04-21 18:54:30 +0800

    桶排序

--- src/com/joker/ContainsDuplicateIII.java
+++ src/com/joker/ContainsDuplicateIII.java
@@ -2,6 +2,9 @@
 
 package com.joker;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 220. Contains Duplicate III
  * Given an array of integers, find out whether there are two distinct indices i and j in the array such that the absolute difference between nums[i] and nums[j] is at most t and the absolute difference between i and j is at most k.
@@ -21,6 +24,33 @@ public class ContainsDuplicateIII {
                 if (Math.abs((long) nums[i] - nums[j]) <= t)
                     return true;
             }
+        }
+        return false;
+    }
+
+
+    /**
+     * 脑容量不够啊！！！
+     * @param nums
+     * @param k
+     * @param t
+     * @return
+     */
+    public static boolean containsNearbyAlmostDuplicateBetter(int[] nums, int k, int t) {
+        if (k < 1 || t < 0) return false;
+        Map<Long, Long> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            long remappedNum = (long) nums[i] - Integer.MIN_VALUE;
+            long bucket = remappedNum / ((long) t + 1);
+            if (map.containsKey(bucket)
+                    || (map.containsKey(bucket - 1) && remappedNum - map.get(bucket - 1) <= t)
+                    || (map.containsKey(bucket + 1) && map.get(bucket + 1) - remappedNum <= t))
+                return true;
+            if (map.entrySet().size() >= k) {
+                long lastBucket = ((long) nums[i - k] - Integer.MIN_VALUE) / ((long) t + 1);
+                map.remove(lastBucket);
+            }
+            map.put(bucket, remappedNum);
         }
         return false;
     }
